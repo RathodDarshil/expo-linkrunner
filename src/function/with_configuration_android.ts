93d5a9ebda8a2591205015754f8d4a144609fa79
Shofiya2003 <shofiyabootwala@gmail.com>
2025-07-24 04:19:05 +0530

    refactor: remove gradle repository configuration from Android setup in with_configuration_android.ts

--- src/function/with_configuration_android.ts
+++ src/function/with_configuration_android.ts
@@ -1,4 +1,4 @@
-import { ConfigPlugin, withProjectBuildGradle } from '@expo/config-plugins'
+import { ConfigPlugin } from '@expo/config-plugins'
 import { ExpoConfig } from '@expo/config-types'
 
 import { LinkrunnerConfiguration } from '../type/configuration'
@@ -7,36 +7,6 @@ const withConfiguration_Android: ConfigPlugin<LinkrunnerConfiguration> = (
     expoConfig: ExpoConfig,
     _configuration: LinkrunnerConfiguration,
 ): ExpoConfig => {
-    // Add gradle repository for rn-linkrunner
-    expoConfig = withProjectBuildGradle(expoConfig, (config) => {
-        
-        if(config.modResults.language == 'groovy') {
-            // Check if the repository is already added
-            if (!config.modResults.contents.includes('maven { url "https://repo1.maven.org/maven2" }')) {
-                config.modResults.contents +=`
-allprojects {
-    repositories {
-        maven { url "https://repo1.maven.org/maven2" }
-    }
-}
-`;
-            }
-        } else {
-            // Check if the repository is already added
-            if (!config.modResults.contents.includes('maven("https://repo1.maven.org/maven2")')) {
-                config.modResults.contents +=`
-allprojects {
-    repositories {
-        maven("https://repo1.maven.org/maven2")
-    }
-}
-`;
-            }
-        }
-
-        return config;
-    });
-
     return expoConfig
 }
 
